988e0e3403167f5865adc1d8924679fcaf6dc883
HodakaSakamoto <2cdi1130@cc.u-tokai.ac.jp>
2025-09-16 00:55:18 +0900

    `BoardStateND` オブジェクトに対してスライスを用いて値を取得する機能を実装. 2次元盤面状況に特化した `BoardState2D` を実装.
    TODO: `BoardState2D` のインデックス指定について, `[2,:]` のように値を `int`
    と `slice` を複合させて値を取得する機能を実装.

--- tkinterboardgame/board/state.py
+++ tkinterboardgame/board/state.py
@@ -1,60 +1,96 @@
 from typing import TypeVar, Generic, TypedDict
 
 
-T = TypeVar("T")
-type StateDimension = list[T | None | StateDimension]
+_T = TypeVar("_T")
+type _StateDimension = list[_T | None | _StateDimension]
 
-class IndexErrorResult(TypedDict):
+class _IndexErrorResult(TypedDict):
     ok: bool
     errors: tuple[Exception]
 
 
-class BoardNdState(Generic[T]):
+class BoardStateND(Generic[_T]):
+    """N次元の盤面状況を保持するための多次元配列クラス."""
 
     def __init__(self, *dimension_sizes: int):
+        """コンストラクタ
+        
+        Args:
+            *dimension_size(int): 各次元の要素数. 最低でも1次元は定義しなければエラーとなる.
+        """
         if len(dimension_sizes) == 0:
             raise ValueError("BoardNdState needs dimension size.")
 
-        self.__state: StateDimension = self.__create_one_dimension(dimension_sizes)
-        self.__dimension_sizes: tuple[int] = dimension_sizes
+        self._state: _StateDimension = self.__create_one_dimension(dimension_sizes)
+        self.__dimension_size: tuple[int] = dimension_sizes
         
-    def __create_one_dimension(self, dimension_sizes: tuple[int]) -> StateDimension | None:
+    def __create_one_dimension(self, dimension_sizes: tuple[int]) -> _StateDimension | None:
+        """各次元のリストを再帰的に作成するメソッド.  
+        
+        初期の要素の値は `None` となる."""
         if len(dimension_sizes) > 1:
-            target: StateDimension = []
-            for d in dimension_sizes:
+            target: _StateDimension = []
+            for _ in range(dimension_sizes[0]):
                 target.append(self.__create_one_dimension(dimension_sizes[1:]))
             return target
         
         if len(dimension_sizes) == 1:
             return [None for _ in range(dimension_sizes[0])]
         
-        raise ValueError(f"a recieved tuple is incorrect. dimension_sizes: {dimension_sizes}")
+        raise ValueError(f"a received tuple is incorrect. dimension_sizes: {dimension_sizes}")
     
-    def __getitem__(self, indexes: tuple[int] | int) -> None | T:
-        """[1, 3]のような値の取得を実装"""
+    def __getitem__(self, indexes: tuple[int] | int | tuple[slice] | slice) -> None | _T:
+        """[1, 3], [1:, 3:5]のような値の取得を実装  
+        
+        TODO: [1, 1:] のようなintとsliceの複合のインデックス指定に対応
+        """
+        
+        match indexes:
+            case tuple():
+                match indexes[0]:
+                    case int(): return self.__get_value_with_tuple_or_int(indexes)
+                    case slice(): return self.__get_values_with_slice(indexes, self._state)
+            case int():
+                return self.__get_value_with_tuple_or_int(indexes)
+            case slice():
+                return self.__get_values_with_slice(indexes, self._state)
+    
+    def __get_value_with_tuple_or_int(self, indexes: tuple[int], int) -> None | _T:
+        """intのインデックス指定で値を取得するメソッド"""
+        
         if type(indexes) == int:
             indexes = tuple(indexes)
         
         check_result = self.__check_index_error(indexes)
         if not check_result["ok"]:
-            raise ExceptionGroup("some error happend about index", check_result["errors"])
+            raise ExceptionGroup("some error happened about index", check_result["errors"])
 
-        target: StateDimension | None | T = self.__state
+        target: _StateDimension | None | _T = self._state
         for idx in indexes:
             if type(target) != list:
                 raise ValueError("some error happened in searching")
             target = target[idx]
         return target
     
-    def __setitem__(self, indexes: tuple[int] | int, value: T | None):
+    def __get_values_with_slice(self, index_slices: tuple[slice], target_list: list[_StateDimension | _T | None]):
+        """sliceのインデックス指定で値を取得するメソッド"""
+        if len(index_slices) == 1: return target_list[index_slices[0]]
+
+        result = []
+        original_lists = target_list[index_slices[0]]
+        for l in original_lists:
+            result.append(self.__get_values_with_slice(index_slices[1:], l))
+        return result
+    
+    def __setitem__(self, indexes: tuple[int] | int, value: _T | None):
         if type(indexes) == int:
             indexes = (indexes,)
         
         check_result = self.__check_index_error(indexes)
         if not check_result["ok"]:
-            raise ExceptionGroup("some error happend about index", check_result["errors"])
+            raise ExceptionGroup("some error happened about index", check_result["errors"])
         
-        target: StateDimension | None | T = self.__state
+        target: _StateDimension | None | _T = self._state
         for idx in indexes[:-1]:
             if type(target) != list:
                 raise ValueError("some error happened in searching: 1")
@@ -62,38 +98,56 @@ class BoardNdState(Generic[T]):
         if type(target) != list:
             raise ValueError("some error happened in searching: 2")
         target[indexes[-1]] = value
-        
     
-    def __check_index_error(self, indexes: tuple[int]) -> IndexErrorResult:
+    def __check_index_error(self, indexes: tuple[int] | tuple[slice]) -> _IndexErrorResult:
         """インデックスの指定に不正がないか判定し, その結果を返すメソッド"""
         errors: list[Exception] = []
-        if len(indexes) != len(self.__dimension_sizes):
-            errors.append(IndexError(f"recieved invalid index: {indexes}"))
-        for idx, d in zip(indexes, self.__dimension_sizes):
-            if idx < 0 or d < idx: errors.append(IndexError(f" index {idx} is out of range."))
-        result: IndexErrorResult = {
+        if len(indexes) != len(self.__dimension_size):
+            errors.append(IndexError(f"received invalid index: {indexes}"))
+
+        if isinstance(indexes[0], int):
+            for idx, d in zip(indexes, self.__dimension_size):
+                if idx < 0 or d < idx: errors.append(IndexError(f" index {idx} is out of range."))
+
+        result: _IndexErrorResult = {
             "ok": len(errors) == 0,
             "errors": errors,
         }
         return result
     
     def __str__(self) -> str:
-        return str(self.__state)
+        return str(self._state)
     
     def __repr__(self) -> str:
         info = {
-            "dimension": str(self.__dimension_sizes),
-            "state": str(self.__state),
+            "dimension": str(self.__dimension_size),
+            "state": str(self._state),
         }
         return str(info)
     
     def __iter__(self):
         # すべての次元を平坦化した一つのリストとしてイテレートする
-        if len(self.__dimension_sizes) == 1:
-            return iter(self.__state)
-        return iter(sum(self.__state, []))
+        if len(self.__dimension_size) == 1:
+            return iter(self._state)
+        return iter(sum(self._state, []))
+    
+    @property
+    def dimension_size(self):
+        return self.__dimension_size
 
 
-state: BoardNdState[int] = BoardNdState(3)
-state[1] = 1
-print(state[1:])
+class BoardState2D(BoardStateND):
+    def __init__(self, x: int, y: int):
+        super().__init__(x, y)
+    
+    @property
+    def rows(self) -> tuple[tuple[_T, None]]:
+        return tuple(map(lambda row: tuple(row), self._state))
+    
+    @property
+    def columns(self) -> tuple[tuple[_T, None]]:
+        sorted_list = [[] for _ in range(self.dimension_size[1])]
+        for row in self._state[0]:
+            for col in self._state[1]:
+                sorted_list[col][row]
+        return tuple(map(lambda col: tuple(col), self._state))
